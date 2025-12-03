139d6ee87a7286122630fce5344f3b5076c5249c
wangchaochao <wangchaochao@58.com>
2025-12-03 14:31:58 +0800

    项目初始化

---
+++ src/test/java/com/example/buzzer1203/Buzzer1203ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buzzer1203;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buzzer1203ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
